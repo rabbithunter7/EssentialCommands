8ddae21bd6a961248b6aacaf9e82a402470fe203
Jordon Kane <kanejord99@outlook.com>
2023-07-09 20:49:57 -0400

    Removed Inventory events from Listener

--- src/main/java/com/essentialcommands/listeners/Listener.java
+++ src/main/java/com/essentialcommands/listeners/Listener.java
@@ -12,28 +12,4 @@ import java.util.HashMap;
 
 public class Listener implements org.bukkit.event.Listener {
 
-    public static HashMap<String, String> backpacks = new HashMap<String, String>();
-
-    @EventHandler
-    public void onInventoryOpEvent(InventoryOpenEvent event) throws IOException {
-        if (event.getPlayer().getOpenInventory().countSlots() == 36){
-
-            InventoryManager invMn = new InventoryManager();
-
-            invMn.saveInventory((Player) event.getPlayer().getOpenInventory());
-            System.out.println("Added "  + event.getPlayer().getName() + " to hashmap.");
-        }
-    }
-
-    @EventHandler
-    public void onInventoryCloseEvent(InventoryCloseEvent event) throws IOException {
-        if (event.getPlayer().getOpenInventory().countSlots() == 36){
-
-            InventoryManager invMn = new InventoryManager();
-
-            invMn.saveInventory((Player) event.getPlayer().getOpenInventory());
-            System.out.println("Saved inventory of player (" + event.getPlayer().getName() + ").");
-            System.out.println("Removed " + event.getPlayer().getName() + " to hashmap.");
-        }
-    }
 }
